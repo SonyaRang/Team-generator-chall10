f3b4618b777a2f20ea1f90c928f9df694f61a32d
Sonz <sonyarangraj@msn.com>
2021-05-30 14:32:46 -0500

    first commit

---
+++ Lib/render.js
@@ -0,0 +1,14 @@
+function generatedHTML(employee) {
+    const employeeDiv = `
+    <div>
+        <h1>${employee.name}</h1>
+        <p>${emplyee.id}</p>
+        `
+
+        switch(employee.getRole)
+
+
+
+
+
+}
