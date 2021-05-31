c9997c0c6000cf2ffb8adc421dd96f591770a710
Sonz <sonyarangraj@msn.com>
2021-05-30 21:04:14 -0500

    changes added toindex.js

--- Lib/Intern.js
+++ Lib/Intern.js
@@ -1,4 +1,17 @@
-const Employee = require( './Employee');
-class Intern extends Employee{
-    constructor()
-}
+const Employee = require('./Employee')
+const { ModuleMocker } = require("jest-mock");
+
+class Intern extends Employee {
+    constructor(name, id, email, school) {
+        super(name, id, email);
+        this.school = school;
+    }
+    getRole() {
+        return 'Intern';
+    }
+    getSchool() {
+        return this.school;
+    }
+};
+
+module.exports = Intern;
---
+++ index.js
@@ -0,0 +1,99 @@
+const Engineer = require("./Engineer");
+const Intern = require("./Intern");
+const Manager = require("./Manager");
+
+module.export = ("Engineer, Intern, Manager, Employee");
+
+const {Manager, Engineer, Intern, Employee} = require('./Lib')
+
+//questions
+const questions = [
+    {
+        type: 'input',
+        name: 'eName',
+        message: 'What is your name?',
+        default:'Name',
+        validate: function(answer) {
+            if (answer < 1) {
+                return console.log('Enter full name');
+            } else {
+                return true;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is your ID?',
+        default: 'ID',
+        validate: function(answer) {
+            if (answer < 1) {
+                return console.log('Enter employee ID');
+            } else {
+                return true;
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?',
+        default: 'Email',
+        validate: function(answer) {
+            if (answer < 1) {
+                return console.log('Enter your email');
+            } else {
+                return true;
+            }
+        }
+    },
+
+    //roles
+    {
+        types: 'list',
+        name: 'role',
+        message: 'What is your role in the company',
+        default: 'Role',
+        choices: ['Manager', 'Engineer', 'Intern']
+    },
+
+// questions for diff roles
+
+{
+    type: 'input',
+    name: 'offNumber',
+    message: 'Enter your office number',
+    when: (input) => input.roles === 'Manager'
+},
+
+{
+    type: 'input',
+    name: 'GitHub',
+    message: 'Enter your GitHub username',
+    when: (input) => input.roles === 'Engineer'
+},
+
+{
+    type:'input',
+    name: 'school',
+    message: 'Enter your school name',
+    when: (input) => input.roles === 'Intern'
+}
+];
+
+//functions to create roles
+//prompt
+function addEmployee() {
+    inquirer.promt(questions)
+    .then(function(answer) {
+        console.log(answers);
+    })
+};
+
+//managers
+//engineers
+//interns
+//HTML page
+
+addEmloyee();
